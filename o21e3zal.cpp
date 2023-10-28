655aebdef3f5609a9ae4f47575f636debc8a073a
Anukia <59363243+Anukia@users.noreply.github.com>
2023-10-28 08:16:02 +0200

    Add files via upload

---
+++ o21e3zal.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+#define MAXN 1000001
+#define INF 9999999999LL
+
+long long tab[MAXN], dp[MAXN];
+
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    int n, s;
+    cin >> n >> s;
+
+    for(int i=1; i<=n; i++) 
+    { 
+        cin >> tab[i];
+    }
+
+    for(int i=2; i<=n; i++) 
+    {
+        if(tab[i-1] >= tab[i]) 
+        {
+            tab[i] = tab[i-1] + 1;
+        }
+    }
+        
+    int load = 0;
+    for(int i=1; i<=n; i++)
+    {
+        dp[i] = INF;
+        int trains = i-load-1;
+
+        while(load < i && max(dp[load]+trains, tab[i]) + trains + 2*s < dp[i])
+        {
+            dp[i] = max(dp[load]+trains, tab[i]) + trains + 2*s;
+            load++; trains--;
+        }
+        load--;
+    }
+
+    cout << dp[n] << "\n";
+    return 0;
+}
